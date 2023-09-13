2b854571fc07cf15e59458b0df241275cd8717e7
Og-Miles <emmanuelogbuji5@gmail.com>
2023-09-13 12:05:01 +0100

    slug image

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -13,7 +13,14 @@ const PreviewProvider = dynamic(() => import("@/components/PreviewProvider"));
 export const postQuery = groq`
   *[_type == "post" && slug.current == $slug][0]{
     title,
-    mainImage,
+    mainImage {
+      _type,
+      alt,
+      asset {
+        _ref,
+        _type
+      }
+    },
     body,
     author->,
     description,
