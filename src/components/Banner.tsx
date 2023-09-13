f07bc4fd0380c137185f5cb0d38eeff8097140a8
Og-Miles <emmanuelogbuji5@gmail.com>
2023-09-13 02:05:46 +0100

    algolia setup

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,38 +1,8 @@
 import React, { useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { SocialIcon } from "react-social-icons";
-import algoliasearch from "algoliasearch/lite";
 
 function Banner() {
-  // Initialize state within the component function
-  const [searchQuery, setSearchQuery] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-
-  const searchClient = algoliasearch(
-    "QAJQY1OUMM",
-    "81cc5249d31ecd1cd4b23a13e1f4698d"
-  );
-  const searchIndex = searchClient.initIndex("Post");
-
-  const searchBlog = async (query) => {
-    try {
-      const { hits } = await searchIndex.search(query, {
-        attributesToHighlight: ["post.title"], // Highlight matching terms in titles
-      });
-      return hits;
-    } catch (error) {
-      console.error("Error searching:", error);
-      return [];
-    }
-  };
-
-  const handleSearchInputChange = async (event) => {
-    const query = event.target.value;
-    setSearchQuery(query);
-    const results = await searchBlog(query);
-    setSearchResults(results);
-  };
-
   return (
     <div className='flex flex-col lg:flex-row justify-between px-10 py-5 space-x-2 max-w-7xl mx-auto'>
       <div>
@@ -48,19 +18,7 @@ function Banner() {
             type='text'
             placeholder='Search'
             className='border-none outline-none placeholder-black px-5 py-2 items-center bg-transparent'
-            value={searchQuery}
-            onChange={handleSearchInputChange}
           />
-        </div>
-
-        {/* Display search results */}
-        <div>
-          {searchResults.map((result) => (
-            <div key={result.objectID}>
-              <h2>{result.title}</h2>
-              <p>{result.content}</p>
-            </div>
-          ))}
         </div>
       </div>
 
---
+++ src/pages/api/update-algolia.ts
@@ -0,0 +1,41 @@
+import algoliasearch from 'algoliasearch/lite';
+import sanityclient from '@sanity/client'
+import indexer, { flattenBlocks } from 'sanity-algolia';
+
+const algolia = algoliasearch('DMWGABW9JT', '3e1c623ff93d694a4f8c92fb7d3076b7');
+const sanity = sanityclient({
+  projectId: '98z6fzay',
+  dataset: 'production',
+  // Other Sanity configuration options
+});
+
+export default function handler(req:any, res:any) {
+  const sanityAlgolia = indexer(
+    {
+      post: {
+        index: algolia.initIndex('posts'),
+      },
+    },
+    (document) => {
+      switch (document._type) {
+        case 'post':
+          return {
+            title: document.title,
+            path: document.slug.current,
+            publishedAt: document.publishedAt,
+            excerpt: flattenBlocks(document.excerpt),
+          };
+        default:
+          throw new Error(`Unknown type: ${document._type}`);
+      }
+    }
+  );
+
+  return sanityAlgolia
+    .webhookSync(sanity, req.body)
+    .then(() => res.status(200).send('Webhook processed successfully'))
+    .catch((error) => {
+      console.error('Webhook error:', error);
+      res.status(500).send('Webhook error');
+    });
+}
