a544c9cb8cc28a489b74f380861ce50413a59fb9
Og-Miles <emmanuelogbuji5@gmail.com>
2023-09-13 02:30:13 +0100

    algolia search

--- src/pages/api/update-algolia.ts
+++ src/pages/api/update-algolia.ts
@@ -1,4 +1,4 @@
-import algoliasearch from 'algoliasearch/lite';
+import algoliasearch from 'algoliasearch';
 import sanityclient from '@sanity/client'
 import indexer, { flattenBlocks } from 'sanity-algolia';
 
