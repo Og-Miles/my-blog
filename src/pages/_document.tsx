e86bc67e6980a1e0ca407565f3f159194fed0512
Og-Miles <emmanuelogbuji5@gmail.com>
2023-09-21 12:06:49 +0100

    header icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+export const metadata = {
+  title: "Moore Blog",
+  description: "The Blog for you",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
+};
+
 export default function Document() {
   return (
     <Html lang='en'>
